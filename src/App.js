965f57520f0feb3fb25e257bf8d9dea170ebdf0c
Hiter Harris <hiterharris@comcast.net>
2019-11-04 17:37:03 -0500

    adding note to database

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Note from './Note/Note';
 import Nav from './Nav/Nav';
 import NoteForm from './NoteForm/NoteForm';
 import DB_CONFIG from './Config/config';
+import 'firebase/database';
 import firebase from 'firebase';
 
 export default class App extends Component {
@@ -12,46 +13,61 @@ export default class App extends Component {
     super(props);
     this.addNote = this.addNote.bind(this);
 
-    this.app = firebase.initializeApp(DB_CONFIG);
-    this.db = this.app.database().ref().child('notes');
+    if (!firebase.apps.length) {
+      try {
+          // firebase.initializeApp(DB_CONFIG)
+          this.app = firebase.initializeApp(DB_CONFIG);
+          this.db = this.app.database().ref().child('notes');
+      } catch (err) {
+          console.error('Firebase initialization error raised', err.stack)
+      }
+  }
 
-    // Setting up the state of the component
     this.state = {
       notes: [],
       }
     }
 
-  addNote(note) {
-    // Push note on to the notes array
-    const previousNotes = this.state.notes;
-    previousNotes.push({ id: previousNotes.length + 1, noteContent: note });
+    componentWillMount() {
+      const previousNotes = this.state.notes;
+      if (!firebase.apps.length) {
+        try {
+          this.database.on('child_added', snap => { // snapshot of previous notes
+            previousNotes.push({
+              id: snap.key,
+              noteContent: snap.val().noteContent,
+            });
     
-    this.setState({
-      notes: previousNotes,
-    });
-  }
+            this.setState({
+              notes: previousNotes,
+            })
+          });
+    
+        } catch (err) {
+            console.error('Firebase initialization error raised', err.stack)
+        }
+      }
+    }
 
-  render() {
+    addNote(note) {
+      // this.database.push().set({ noteContent: note});
+    }
 
-    return (
-      <div className="app">
-
-        <Nav />
-
-        <div className="notesBody">
-            {
-              this.state.notes.map( (note) => {
-                return (
-                  <Note noteContent={note.noteContent} noteId={note.id} key={note.id} />
-                );
-              })
-            } 
+    render() {
+      return (
+        <div className="app">
+          <Nav />
+          <div className="notesBody">
+              {
+                this.state.notes.map( (note) => {
+                  return (
+                    <Note noteContent={note.noteContent} noteId={note.id} key={note.id} />
+                  );
+                })
+              } 
+          </div>
+          <NoteForm addNote={this.addNote} />
         </div>
-
-        <NoteForm addNote={this.addNote} />
-
-    </div>
-    );
-
+      );
   }
 }
