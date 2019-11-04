e7f21df7a1ad0bfdf9b286ebf36e6e0205852d97
Hiter Harris <hiterharris@comcast.net>
2019-11-04 15:29:38 -0500

    project, component setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Note from './Note/Note';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        { id: 1, noteContent: "Note 1 here!" },
+        { id: 2, noteContent: "Note 2 here!" },
+      ],
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="app">
+
+        <div className="notesHeader">
+          <div className="heading">React & Firebase Notes App</div>
+        </div>
+
+        <div className="notesBody">
+          {
+            this.state.notes.map( (note) => {
+              return (
+                <Note noteContent={note.noteContent} noteId={note.id} key={note.id} />
+              );
+            })
+          }
+        </div>
+
+        <div className="notesFooter">
+          Footer will go here...
+        </div>
+        
     </div>
-  );
-}
+    );
 
-export default App;
+  }
+}
---
+++ src/Note/Note.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Note.css';
+import PropTypes from 'prop-types';
+
+export default class Note extends Component {
+    constructor(props) {
+        super(props);
+        this.noteContent = props.noteContent;
+        this.noteId = props.noteId;
+    }
+    render() {
+        return (
+            <div className="note fade-in">
+                <p className="noteContent">{ this.noteContent }</p>
+            </div>
+        );
+    }
+}
+
+Note.propTypes = {
+  noteContent: PropTypes.string,
+}
